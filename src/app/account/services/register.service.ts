677172e29283e1af9c09354849230ab1db1ff413
Domingos Latorre <domingos.latorre@gmail.com>
2023-11-16 10:49:51 -0300

    chore: config app environments

--- src/app/account/services/register.service.ts
+++ src/app/account/services/register.service.ts
@@ -3,13 +3,14 @@ import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { User } from "../models/user.model";
 import { UserCreateForm } from "../models/user-register.model";
+import { environment } from "../../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterService {
   http = inject(HttpClient);
-  apiUrl = 'http://localhost:8080/register';
+  apiUrl = environment.apiUrl + '/register';
 
   register(userCreateForm: UserCreateForm): Observable<User> {
     return this.http.post<User>(this.apiUrl, userCreateForm);
